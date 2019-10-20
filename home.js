13e9549f4ec6dee41ecc85ada18c53252706ff60
jgo98 <gojoshua98@gmail.com>
2019-10-20 14:45:27 -0700

    slideshow added

--- home.js
+++ home.js
@@ -1,15 +1,29 @@
-var slideIndex = 0;
-carousel();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-/* https://www.w3schools.com/w3css/w3css_slideshow.asp */
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-        x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* https://www.w3schools.com/howto/howto_js_slideshow.asp */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slideshow-image");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+} 
